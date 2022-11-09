edd597574252e2fc3147d675e129bc73a7e84799
davien21 <chidiebereekennia@gmail.com>
2022-11-09 22:46:37 +0100

    --test flw webhook

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const candidateRouter = require("./candidates");
 const blogRouter = require("./blog");
-const mediaRouter = require("./media");
+const webhookRouter = require("./webhooks");
+const allDataRouter = require("./all");
 const registrationsRouter = require("./registrations");
 const eventRouter = require("./event");
 const couponsRouter = require("./coupons");
-const allDataRouter = require("./all");
 
 module.exports = () => {
   router.use(userRouter());
   router.use(candidateRouter());
   router.use(blogRouter());
-  router.use(mediaRouter());
+  router.use(webhookRouter());
   router.use(allDataRouter());
   router.use(registrationsRouter());
   router.use(eventRouter());
---
+++ src/routes/webhooks.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+
+const response = require("../utils/response");
+const fs = require("fs");
+const registrationService = require("../services/registration");
+const { BadRequestError, UnAuthorizedError } = require("../lib/errors");
+
+const Flutterwave = require("flutterwave-node-v3");
+const flw = new Flutterwave(process.env.FLW_PUBLIC, process.env.FLW_SECRET);
+
+const getFlutterwaveWebhook = (req) => {
+  console.log("FLW Webhook", req.body);
+
+  // If you specified a secret hash, check for the signature
+  const secretHash = process.env.FLW_SECRET_HASH;
+  const signature = req.headers["verif-hash"];
+  if (!signature || signature !== secretHash)
+    throw new UnAuthorizedError("Invalid signature");
+
+  fs.writeFileSync("flw-webhook.json", JSON.stringify(req.body));
+  return req.body;
+};
+
+module.exports = function () {
+  router.post("/webhooks/flutterwave", async (req, res) => {
+    console.log("REQUEST:\n", req);
+    const payload = getFlutterwaveWebhook(req);
+
+    //check if the payment exist in the flutterwave database
+    const validPayment = await flw.Transaction.verify({ id: payload.data.id });
+    if (
+      !validPayment?.data ||
+      validPayment.data.status !== "successful" ||
+      validPayment.data.currency !== "NGN"
+    )
+      throw new BadRequestError("Invalid Payment");
+    console.log("Valid Payment", validPayment);
+
+    const validRegistration = await registrationService.findAndUpdate(
+      {
+        paymentId: validPayment.data.tx_ref,
+        "transaction.amount": validPayment.data.amount,
+      },
+      { transaction: { hasPaid: true } }
+    );
+    console.log("Valid Registration", validRegistration);
+
+    if (!validRegistration) throw new BadRequestError("Invalid Registration");
+
+    res.send(response("Payment Successful"));
+  });
+
+  return router;
+};
